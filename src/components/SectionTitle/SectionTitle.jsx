ee371270ccce65f7b0144d144a87a9e5e9393c5f
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-11-12 17:25:38 +0300

    feat: use memo in title element

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import './SectionTitle.css';
 
-export default function SectionTitle({ title, subtitle }) {
-  return (
-    <>
-      <div className="section-title-container">
-        <h2 className="section-title">{title}</h2>
-        <p className="section-subtitle">
-          {subtitle}
-        </p>
-      </div>
-    </>
-  );
-}
+const SectionTitle = memo(({ title, subtitle }) => (
+  <>
+    <div className="section-title-container">
+      <h2 className="section-title">{title}</h2>
+      <p className="section-subtitle">{subtitle}</p>
+    </div>
+  </>
+));
+
+export default SectionTitle;
 
 SectionTitle.propTypes = {
   title: PropTypes.string.isRequired,
