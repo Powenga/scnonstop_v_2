e5eee65cc08622bc39430b5193a39f9095641761
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-12-29 16:41:46 +0300

    ref: remove inner span in phone link

--- src/components/PhoneLink/PhoneLink.jsx
+++ src/components/PhoneLink/PhoneLink.jsx
@@ -5,7 +5,7 @@ import './PhoneLink.css';
 function PhoneLink({ classes, children }) {
   return (
     <a href="tel:+79508022222" className={`phone-link ${classes && classes}`}>
-      <span className="phone-link__text">{children}</span>
+      {children}
     </a>
   );
 }
